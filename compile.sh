cbac4a3db8a80d6e58b4fd4463c23ffd46ad8479
Owen Jones <owenjonesuk@gmail.com>
2017-03-14 20:43:32 +0000

    Switched to clang

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
-#g++.exe -std=c++11 cell.cpp -c
-#g++.exe -std=c++11 sudokugrid.cpp -c
-#g++.exe -std=c++11 solvesudoku.cpp sudokugrid.o cell.o -o solvesudokugrid
-clang++ -std=c++11 cell.cpp -c
-clang++ -std=c++11 sudokugrid.cpp -c
-clang++ -std=c++11 solvesudoku.cpp sudokugrid.o cell.o -o solvesudokugrid
+#!/bin/bash
+
+COMPILER=clang++
+$COMPILER -std=c++11 cell.cpp -c
+$COMPILER -std=c++11 sudokugrid.cpp -c
+$COMPILER -std=c++11 solvesudoku.cpp sudokugrid.o cell.o -o solvesudokugrid
