1e6e0ce94d2169f525e9743a659c32d341d87d1c
Owen Jones <owenjonesuk@gmail.com>
2017-03-19 20:38:16 +0000

    Removed debugging lines, tested on harder grid, seems to work

--- solvesudoku.cpp
+++ solvesudoku.cpp
@@ -3,24 +3,15 @@
 
 SudokuGrid solveSudokuGrid(SudokuGrid start_grid) {
   std::stack<SudokuGrid> stack;
-  std::cout << "  got here 1" << std::endl;
   stack.push(start_grid);
-  std::cout << "  got here 2" << std::endl;
   while (!stack.empty()) {
-  std::cout << "  got here 3" << std::endl;
     SudokuGrid& grid = stack.top();
-  std::cout << "  got here 4" << std::endl;
     grid.setCellsWithOnePossibleValue();
-  std::cout << "  got here 5" << std::endl;
     if (grid.isCompleted()) {
-  std::cout << "  got here 6" << std::endl;
       break;
     }
-  std::cout << "  got here 7" << std::endl;
     grid.branchAndAddToStack(stack);
-  std::cout << "  got here 8" << std::endl;
   }
-  std::cout << "  got here 9" << std::endl;
   if (stack.empty()) {
     throw "Failed to find a solution";
   }
@@ -28,11 +19,8 @@ SudokuGrid solveSudokuGrid(SudokuGrid start_grid) {
 }
 
 int main(int argv, char* argc[]) {
-  SudokuGrid sudoku_grid("grids/grid_one_cell_missing.txt");
-  std::cout << "got here 1" << std::endl;
+  SudokuGrid sudoku_grid("grids/grid_solvable.txt");
   SudokuGrid completed_grid = solveSudokuGrid(sudoku_grid);
-  std::cout << "got here 2" << std::endl;
   completed_grid.print(std::cout);
-  std::cout << "got here 3" << std::endl;
   return 0;  
 }
